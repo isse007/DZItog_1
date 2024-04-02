3d91d9e8fdf3e2e271025fb240850655feb80ead
Simon Isupov <is_se@mail.ru>
2024-04-02 23:24:34 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void PrintNumber (int M, int N)
+{
+    // System.Console.WriteLine($"Числа от M {M} до N {N}");
+    System.Console.WriteLine($"{M}");
+    if (N==M) 
+    {
+        return;
+    }
+    PrintNumber (M+1,N);
+    
+}
+
+ PrintNumber (5,9);
